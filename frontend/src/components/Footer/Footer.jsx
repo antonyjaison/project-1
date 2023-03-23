11747d3b40bfd2667553e0ee3056ca8886006148
Antony Jaison <antonyjaison639@gmail.com>
2023-03-23 09:03:20 +0530

    footer chair changes to sports

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
             <div className="col-lg-4 col-md-6 col-12 footer_logo">
               <Logo fontSize={44} />
               <p>
-                Selling chairs <br /> since 1990
+                Selling sports items <br /> since 1990
               </p>
             </div>
             <div className="col-lg-4 col-md-6 col-12 mt-4">
